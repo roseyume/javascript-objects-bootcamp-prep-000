619c5f4560b51c9990c5d84bd77e0640c243847b
Rosie Wang <rosiedwang@yahoo.com>
2018-01-10 21:34:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
 var playlist  = new Object({Haha: "SSOA"})
+function updatePlaylist(playlist, artist, song)
+{
+  playlist.assign(playlist, {[artist]: song})
+  return playlist
+}
+function removeFromPlaylist(playlist,artist)
+{
+  playlist.delete
+}
